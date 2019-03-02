2e6aa0d5459e17f82c1dd350b5b9578e5de43f0a
ShenHaiYuDanDan <1119778139@qq.com>
2019-03-02 22:59:13 +0800

    修改DataSourec

--- src/main/java/com/shydd/mybolg/MybolgApplication.java
+++ src/main/java/com/shydd/mybolg/MybolgApplication.java
@@ -1,9 +1,13 @@
 package com.shydd.mybolg;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MybolgApplication {
 
     public static void main(String[] args) {
